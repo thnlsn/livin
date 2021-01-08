ee456a344b139648abf023107758898734cda74c
Thomas Nelson <thnlsn@gmail.com>
2021-01-07 18:05:57 -0800

    sidebar and header empty components created and imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './css/style.css';
 
+// Import Layout Components
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+
 function App() {
-  return <div className='App'></div>;
+  return <div className='container'></div>;
 }
 
 export default App;
