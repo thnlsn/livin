779eb208e2e1fbcde1d6346ad22b5e799834f68d
Thomas Nelson <thnlsn@gmail.com>
2021-01-08 14:02:45 -0800

    story headings and button styled

--- src/components/Story.js
+++ src/components/Story.js
@@ -8,7 +8,9 @@ const Story = ({ story }) => {
       <div className='story__pictures'>Story pictures</div>
       <div className='story__content'>
         <h3 className='heading-3'>{content.heading}</h3>
-        <h2 className='heading-2'>&ldquo;{content.quote}&rdquo;</h2>
+        <h2 className='heading-2 heading-2--sark'>
+          &ldquo;{content.quote}&rdquo;
+        </h2>
         <p className='story__text'>{content.text}</p>
         <button className='btn'>{content.buttonText}</button>
       </div>
