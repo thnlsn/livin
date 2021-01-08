86d4e575e9499690d34f4858b318708ca5e4b22e
Thomas Nelson <thnlsn@gmail.com>
2021-01-08 15:56:03 -0800

    grid layout for the story pictures section to achive overlaying images and widths above 100%

--- src/components/Story.js
+++ src/components/Story.js
@@ -5,7 +5,15 @@ const Story = ({ story }) => {
 
   return (
     <Fragment>
-      <div className='story__pictures'>Story pictures</div>
+      <div
+        className='story__pictures'
+        style={{
+          backgroundImage: `url(${pictures.backgroundImage})`,
+        }}
+      >
+        <img src={pictures.imageOne} alt='Couple' className='story__img--1' />
+        <img src={pictures.imageTwo} alt='Home' className='story__img--2' />
+      </div>
       <div className='story__content'>
         <h3 className='heading-3 u-mb-sm'>{content.heading}</h3>
         <h2 className='heading-2 heading-2--dark u-mb-md'>
